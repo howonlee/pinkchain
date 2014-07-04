271d9a0c16fb3bbe8e931a96a2bf70a276947061
Howon Lee <howon@howonlee.com>
2014-07-04 14:10:36 -0700

    semimarkov model on the non-words

--- noise.py
+++ noise.py
@@ -51,29 +51,32 @@ def pinkwords_to_file(iterpink, N=200):
 	with open('pinknoise_markov_words.txt', 'w') as f:
 		f.write(" ".join(n))
 
-def pink_semimarkov_letters_to_file(iterpink, N=10000):
+def pink_semimarkov_letters_to_file(iterpink, ngram_order=3, N=500):
 	#do third-order markov chain for all this shit
 	print "beginning parsing of corpus..."
 	ct = collections.Counter()
 	with open("total_corpus.txt", "r") as corpus:
 		for line in corpus:
 			line_processed = list(line.lower().translate(string.maketrans("",""), string.punctuation))
+			for ngram in zip(*[line_processed[i:] for i in range(ngram_order)]):
+				ct["".join(ngram)] += 1
 			#now the markov process
-			for char in lin
 	markov_list = ct.items()
 	markov_array = map(operator.itemgetter(1), markov_list)
 	normalizer = math.log(sum(markov_array))
+	print "length of markov list: ", len(markov_list)
 	markov_array = numpy.array(map(lambda x: math.exp(math.log(x) - normalizer), markov_array))
 	word_array = map(operator.itemgetter(0), markov_list)
 	print markov_array
 	print "finished corpus fuckery. beginning generation..."
 	n = list(islice(iterpink(markov_array, word_array), N))
 	print "generated, starting the write..."
-	with open('pinknoise_markov_words.txt', 'w') as f:
-		f.write(" ".join(n))
+	with open('pinknoise_semimarkov_words.txt', 'w') as f:
+		f.write("".join(n))
 
 
 if __name__ == "__main__":
 	#pinknoise_to_file(iterpinks.float_iterpink)
 	#pinkletters_to_file(iterpinks.markov_letter_iterpink)
-	pinkwords_to_file(iterpinks.markov_word_iterpink)
+	#pinkwords_to_file(iterpinks.markov_word_iterpink)
+	pink_semimarkov_letters_to_file(iterpinks.semimarkov_letter_iterpink)
