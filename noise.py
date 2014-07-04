70798200f190493a08eb081599517721fb9fbe80
Howon Lee <howon@howonlee.com>
2014-07-04 12:20:41 -0700

    begin work on making the markov chain according to priors

--- noise.py
+++ noise.py
@@ -60,6 +60,37 @@ def pinkletters_to_file(iterpink, N=10000):
 			f.write("%s" % ff[-1])
 
 def markov_letter_iterpink(depth=20):
+	#put down a dirichlet distribution for the prior letter freqs
+	#draw from the distribution to actually fill out values and smooth and src
+
+	#8.167% a's, 1.492% b's, etc
+	prior = numpy.array([8167, 1492, 2782, 4253, 13000, 2228, 2015, 6094, 6966, 153, 772, 4025, 2406, 6749, 7507, 1929, 95, 5987, 6327, 9056, 2758, 978, 2360, 150, 1974, 74])
+	num_letters = 26
+	#write it out in a matrix, everybody, to find the right axes for numpy calc
+	letters_array = numpy.array(list(string.ascii_lowercase))
+	values = [numpy.random.randn(num_letters) for i in xrange(depth)]
+	smooth = [numpy.random.randn(num_letters) for i in xrange(depth)]
+	source = [numpy.random.randn(num_letters) for i in xrange(depth)]
+	val_sum = sum(values) #across the right dimension
+	i = 0
+	while True:
+		probs = abs(val_sum + smooth[i]) / sum(abs(val_sum + smooth[i]))
+		yield numpy.random.choice(letters_array, p=probs)
+		#yield a sample from the distribution, actually
+		#there's a numpy thing for this
+		i += 1
+		if i == depth:
+			i = 0
+			smooth = [numpy.random.randn(num_letters) for i in xrange(depth)]
+			source = [numpy.random.randn(num_letters) for i in xrange(depth)]
+			continue
+		c = 0
+		while not (i >> c) & 1: #count trailing zeroes
+			c += 1
+		val_sum += source[i] - values[c] #take abs
+		values[c] = source[i] #do right dimension
+
+def markov_letter_iterpink_uniform(depth=20):
 	num_letters = 26
 	#write it out in a matrix, everybody, to find the right axes for numpy calc
 	letters_array = numpy.array(list(string.ascii_lowercase))
